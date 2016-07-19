5fc1bac63ee53f93098fee7c1292a7e29186739f
ghibulo <jirka.hrbek@gmail.com>
2016-07-19 15:48:34 +0200

    vytvoření značkovacího jazyka

--- main.cpp
+++ main.cpp
@@ -5,8 +5,11 @@
 #include <map>
 #include <set>
 #include <stdio.h>
-#include <glib.h>
 #include <vector>
+#include <sys/stat.h>
+#include <climits>
+
+#define DEBUG
 
 using namespace std;
 
@@ -27,12 +30,22 @@ int maxDiv(int n) {
     return 1;
 }
 
-void printMaps(ofstream &where, map<int,set<string>> &words) {
+inline bool existsFile (const std::string& name) {
+  struct stat buffer;
+  return (stat (name.c_str(), &buffer) == 0);
+}
+
+void printMaps(ostream &where, map<int,set<string>> &words, int cnt=INT_MAX) {
+    int c=0;
     for (map<int,set<string> >::iterator it=words.begin(); it!=words.end(); it++) {
 
         where << it->first << ":  " <<endl;
         for (set<string>::iterator vit=(it->second).begin();vit!=(it->second).end();vit++) {
-            where << " - " << *vit << endl;
+            where << " + " << *vit << endl;
+        }
+        if (++c > cnt) {
+            cout << "and so on..."<<endl;
+            return;
         }
 
     }
@@ -56,48 +69,111 @@ vector<string> getWordsOfText(string name) {
 }
 
 
-
-int main()
-
-{
-    vector<string> res = getWordsOfText("hovory.txt");
-    //for(int i=0;i<res.size();i++) cout << res[i]
-/*
-
+map<int,set<string>> createCodedConfiguration(string filewords, string codconf) {
     map<int,set<string>> words;
     map<int,set<string> >::iterator it;
     pair<int,set<string> > intVec;
 
-    string arr = "kápě";
-    int del;
-    cout << keyRead(arr.c_str()) << "... ahoj " <<endl;
-    ifstream inp("ceskapodstjm.txt");
-    //ifstream inp("data.txt");
-    int i=1;
+    string lin; //line of text file = another word
+    int num; //key for group of words
+    int siz; //size of word
+
+    ifstream inp(filewords);
+    int i=1, j=1;
     while (!inp.eof()) {
-        getline(inp,arr);
-        arr.resize(arr.size()-1);
-        //kod = keyRead(arr.c_str());
-        //del = maxDiv(kod);
-        del = i++ % 5000 + 1;
-        //cout << arr << " ... " << kod << " -> " << del << endl;
-        if ( (it =words.find(del) )==words.end()) {
+        getline(inp,lin);
+        lin.resize(lin.size()-1); //msdos format of text file -> delete '\r' character
+
+        if ( (siz = lin.size()) <=4)
+            num = -(j++ % 7 + 1); //short words for 7 special groups (product, add, decimal point, numbers, ascii...)
+        else
+            num = i++ % 5000 + 1;
+
+        if ( (it =words.find(num) )==words.end()) {
             set<string> el;
-            el.insert(arr);
-            words.insert(pair<int,set<string> >(del,el));
+            el.insert(lin);
+            words.insert(pair<int,set<string> >(num,el));
         } else {
-            it->second.insert(arr);
+            it->second.insert(lin);
         }
     }
     inp.close();
-
-    ofstream o("result.txt");
+    ofstream o(codconf);
     printMaps(o,words);
     o.close();
+    return words;
+}
+
+map<int,set<string>> openCodedConfiguration(string filewords) {
+    map<int,set<string>> words;
+    map<int,set<string> >::iterator it;
+    pair<int,set<string> > intVec;
+
+    string lin; //line of text file = another word
+    int ngr;
+
+    ifstream inp(filewords);
+    map<int,set<string> >::iterator actGroup;
+    pair< map<int,set<string> >::iterator, bool> temp;
+    while (!inp.eof()) {
+        getline(inp,lin);
+        if (lin.empty()) continue; //empty lines allowed
+        if (lin[0]!=' ') {
+
+            set<string> el;
+            try {
+            ngr = stoi(lin);
+            temp = words.insert(pair<int,set<string> >(ngr,el));
+            }
+            catch(std::invalid_argument& e){
+                cout << "Name of words group must be a number!" << endl;
+                cout << "lin=" << lin << "ag=" << actGroup->first << endl;
+                exit(1);
+
+            }
+            catch(std::out_of_range& e){
+                cout << "Invalid range for number-name of groups" << endl;
+                exit(2);
+            }
+            catch(...) {
+              // everything else
+              cout << "File conf.txt is corrupted." << endl;
+                exit(2);
+            }
+            actGroup = temp.first;
+        } else {
+            actGroup->second.insert(lin.substr(3));
+        }
+    }
+    inp.close();
+    #ifdef DEBUG
+    printMaps(cout,words,10);
+    #endif
+    return words;
+}
 
 
-    */
+int main()
 
-    //cin >> arr;
+{
+    //vector<string> res = getWordsOfText("hovory.txt");
+    //for(int i=0;i<res.size();i++) cout << res[i]
+    //cout << keyRead(arr.c_str()) << "... ahoj " <<endl;
+            //kod = keyRead(arr.c_str());
+        //del = maxDiv(kod);
+
+    map<int,set<string>> codeConf;
+    if (existsFile("conf.txt")) {
+        codeConf = openCodedConfiguration("conf.txt");
+    } else {
+        if (existsFile("ceskaslova.txt"))
+            codeConf = createCodedConfiguration("ceskaslova.txt", "conf.txt");
+        else {
+            cout << "Can't find conf.txt or ceskaslova.txt" << endl;exit(0);
+        }
+    }
+
+
     return 0;
+
 }
